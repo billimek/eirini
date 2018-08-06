1f7bf7362effe55a643b859bb0144487baddc4cb
Julian Skupnjak <skupnjak@de.ibm.com>
2018-08-06 09:36:59 +0300

    Refactor tests
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- k8s/service_test.go
+++ k8s/service_test.go
@@ -19,7 +19,6 @@ var _ = Describe("Service", func() {
 	var (
 		fakeClient     kubernetes.Interface
 		serviceManager ServiceManager
-		lrps           []*opi.LRP
 	)
 
 	const (
@@ -75,38 +74,6 @@ var _ = Describe("Service", func() {
 			})
 		})
 
-		Context("When deleting a service", func() {
-			BeforeEach(func() {
-				lrps = []*opi.LRP{
-					createLRP("odin", "1234.5"),
-					createLRP("thor", "4567.8"),
-					createLRP("mimir", "9012.3"),
-				}
-			})
-
-			JustBeforeEach(func() {
-				for _, l := range lrps {
-					fakeClient.CoreV1().Services(namespace).Create(toService(l, namespace))
-				}
-			})
-
-			It("deletes the service", func() {
-				err := serviceManager.Delete("odin")
-				Expect(err).ToNot(HaveOccurred())
-
-				services, _ := fakeClient.CoreV1().Services(namespace).List(meta.ListOptions{})
-				Expect(services.Items).To(HaveLen(2))
-				Expect(getServicesNames(services)).To(ConsistOf(eirini.GetInternalServiceName("mimir"), eirini.GetInternalServiceName("thor")))
-			})
-
-			Context("when the service does not exist", func() {
-				It("returns an error", func() {
-					err := serviceManager.Delete("non-existing")
-					Expect(err).To(HaveOccurred())
-				})
-			})
-		})
-
 		Context("When creating a headless service", func() {
 
 			JustBeforeEach(func() {
@@ -135,6 +102,41 @@ var _ = Describe("Service", func() {
 				})
 
 				It("should return an error", func() {
+					Expect(err).To(HaveOccurred())
+				})
+			})
+		})
+
+		Context("When deleting a service", func() {
+
+			var lrps []*opi.LRP
+
+			BeforeEach(func() {
+				lrps = []*opi.LRP{
+					createLRP("odin", "1234.5"),
+					createLRP("thor", "4567.8"),
+					createLRP("mimir", "9012.3"),
+				}
+			})
+
+			JustBeforeEach(func() {
+				for _, l := range lrps {
+					fakeClient.CoreV1().Services(namespace).Create(toService(l, namespace))
+				}
+			})
+
+			It("deletes the service", func() {
+				err := serviceManager.Delete("odin")
+				Expect(err).ToNot(HaveOccurred())
+
+				services, _ := fakeClient.CoreV1().Services(namespace).List(meta.ListOptions{})
+				Expect(services.Items).To(HaveLen(2))
+				Expect(getServicesNames(services)).To(ConsistOf(eirini.GetInternalServiceName("mimir"), eirini.GetInternalServiceName("thor")))
+			})
+
+			Context("when the service does not exist", func() {
+				It("returns an error", func() {
+					err := serviceManager.Delete("non-existing")
 					Expect(err).To(HaveOccurred())
 				})
 			})
